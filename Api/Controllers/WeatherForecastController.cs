ce5a98fcb3e7d63e4ae70696a7aced353abef25c
Roy Gerritse <info@roygerritse.nl>
2019-11-14 00:20:41 +0100

    fix

--- Api/Controllers/WeatherForecastController.cs
+++ Api/Controllers/WeatherForecastController.cs
@@ -11,11 +11,6 @@ namespace Api.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -27,14 +22,6 @@ namespace Api.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             _logger.LogInformation("test");
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
     }
 }
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-=======
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,18 +10,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
->>>>>>> fdb2c5cd4e345a5d612aa4e857940f4dcce5e2d3
 
 namespace Api
 {
     public class Startup
-<<<<<<< HEAD
     {
         private const string ConnectionString = "Host=db;Database=mydb;Username=roy;Password=pwd0123456789";
 
-=======
     {
->>>>>>> fdb2c5cd4e345a5d612aa4e857940f4dcce5e2d3
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -40,13 +28,9 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-<<<<<<< HEAD
             services.AddControllers();
 
             services.AddDbContext<MyContext>(options => options.UseNpgsql(ConnectionString));
-=======
-            services.AddControllers();
->>>>>>> fdb2c5cd4e345a5d612aa4e857940f4dcce5e2d3
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
